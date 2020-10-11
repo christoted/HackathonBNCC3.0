4d3b6649cb9e0d5d7958c8f85817212304229808
garrystevens007 <49149545+garrystevens007@users.noreply.github.com>
2020-10-12 06:00:43 +0700

    Update-Autofill

--- app/src/main/java/com/example/nontoninaja/Activity/PaymentActivity.java
+++ app/src/main/java/com/example/nontoninaja/Activity/PaymentActivity.java
@@ -7,15 +7,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.nontoninaja.Model.MyTicket;
 import com.example.nontoninaja.Model.MyTicketWithQty;
 import com.example.nontoninaja.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import java.text.NumberFormat;
 import java.util.Locale;
+
+import javax.annotation.Nullable;
 
 public class PaymentActivity extends AppCompatActivity {
 
@@ -23,16 +35,62 @@ public class PaymentActivity extends AppCompatActivity {
     CardView cardReguler,cardVip,cardVvip;
     TextView eventTitle,eventCategory,eventDate,eventTime;
     FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
+    DatabaseReference databaseReference;
+    EditText profileName, profilePhoneNumber, profileEmail, profileAddress;
+    String userID, profileGoogle, emailGoogle;
 
     TextView priceReguler,priceVip,priceVvip,qtyReguler,qtyVip,qtyVvip,tvtotalPrice;
+
+    private void getCurrData(){
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if (acct != null) {
+            profileGoogle = acct.getDisplayName();
+            emailGoogle = acct.getEmail();
+
+            Log.d("DISPLAYED", "Name: " + profileGoogle);
+            Log.d("DISPLAYED", "EMAIL: " + emailGoogle);
+        }
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("users");
+        DocumentReference documentReference = firebaseFirestore.collection("users").document(userID);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                if(documentSnapshot.getString("name") != null){
+                    profileName.setText(documentSnapshot.getString("name"));
+                    profileEmail.setText(documentSnapshot.getString("email"));
+                    profilePhoneNumber.setText(documentSnapshot.getString("phone"));
+                    profileAddress.setText(documentSnapshot.getString("address"));
+                }else{
+                    profileName.setText(profileGoogle);
+                    profileEmail.setText(emailGoogle);
+                }
+
+            }
+        });
+    }
+
+    private void initData(){
+        profileName = findViewById(R.id.id_activity_payment_name);
+        profileEmail = findViewById(R.id.id_activity_payment_email);
+        profilePhoneNumber = findViewById(R.id.id_activity_payment_number);
+        profileAddress = findViewById(R.id.id_activity_payment_address);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+        initData();
         firebaseAuth = FirebaseAuth.getInstance();
-
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        userID = firebaseAuth.getCurrentUser().getUid();
         Log.d("Nama", firebaseAuth.getCurrentUser().getDisplayName());
-        
+        getCurrData();
+
+
         cardReguler = findViewById(R.id.cardView4);
         cardVip = findViewById(R.id.cardView5);
         cardVvip = findViewById(R.id.cardView6);
