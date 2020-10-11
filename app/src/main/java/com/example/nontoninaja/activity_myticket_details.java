c50361e1f15464d983856d4c0412bd2e530cb18b
Arvin Indrawan <48991518+ArvinVin@users.noreply.github.com>
2020-10-12 06:35:46 +0700

    Update_Fix

---
+++ app/src/main/java/com/example/nontoninaja/activity_myticket_details.java
@@ -0,0 +1,14 @@
+package com.example.nontoninaja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_myticket_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myticket_details);
+    }
+}
